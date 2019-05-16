72ff94b4fd96256f779e08196fa74daf250b31a6
Chauvin <timotheechauvin1996@gmail.com>
2019-05-16 02:17:30 +0200

    Clean a bit the custom viewer

--- src/Mesh.h
+++ src/Mesh.h
@@ -1,29 +1,29 @@
 #ifndef PROJECTMESH_H
 #define PROJECTMESH_H
 
-#include <vector>
 #include "point3.h"
+#include <vector>
 
-
-
-struct Vertex{
-    point3d p;
-    Vertex() {}
-    Vertex(double x , double y , double z) : p(x,y,z) {}
-    double & operator [] (unsigned int c) { return p[c]; }
-    double operator [] (unsigned int c) const { return p[c]; }
+struct Vertex {
+  point3d p;
+  Vertex() {}
+  Vertex(double x, double y, double z) : p(x, y, z) {}
+  double &operator[](unsigned int c) { return p[c]; }
+  double operator[](unsigned int c) const { return p[c]; }
 };
-struct Triangle{
-    unsigned int corners[3];
-    unsigned int & operator [] (unsigned int c) { return corners[c]; }
-    unsigned int operator [] (unsigned int c) const { return corners[c]; }
-    unsigned int size() const { return 3 ; }
+struct Triangle {
+  unsigned int corners[3];
+  unsigned int &operator[](unsigned int c) { return corners[c]; }
+  unsigned int operator[](unsigned int c) const { return corners[c]; }
+  unsigned int size() const { return 3; }
 };
-struct Mesh{
-    std::vector< Vertex > vertices;
-    std::vector< Triangle > triangles;
+struct Mesh {
+  std::vector<Vertex> vertices;
+  std::vector<Triangle> triangles;
+  void clear() {
+    vertices.clear();
+    triangles.clear();
+  };
 };
-
-
 
 #endif // PROJECTMESH_H
